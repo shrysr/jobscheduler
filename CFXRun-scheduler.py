f0dc9a1415687ef6585a771c8162dc9402bd90f1
sragavan88 <sragavan88@users.noreply.github.com>
2015-04-13 14:00:02 +0530

    Update CFXRun-scheduler.py

--- CFXRun-scheduler.py
+++ CFXRun-scheduler.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Name:        CFX job Scheduler
-# Purpose:     Program to run priority level and normal level simulations in a First-in-first-out manner
+# Purpose:     Program to run simulations of ANSYS CFX in a First-in-first-out manner, with different folders serving as different priority levels.
 #
 # Author:      Shreyas Ragavan
 #
@@ -22,12 +22,15 @@ import subprocess as sp
 import time as t
 import datetime as dt
 import glob as glob
+import re
+import shutil as sh
 
 #Setting the required Folder paths/locations. Use this list to change paths to folders.
-status_loc=r"Q:\Queueing_sys\admin\status2.txt"
-status_other_loc=r"Q:\Queueing_sys\admin\status1.txt"
+MP_working_loc=r"Q:\Queueing_sys\working_loc\MNP_working_loc"
+status_loc=r"Q:\Queueing_sys\admin\status1.txt"
+single_cluster_switch=r"Q:\Queueing_sys\admin\onlysmall_CL1.txt"
 excess_size_loc=r"Q:\Queueing_sys\Excess_size_NotInUse"
-pause_set_loc=r"Q:\Queueing_sys\admin\pause2.txt"
+pause_set_loc=r"Q:\Queueing_sys\admin\pause1.txt"
 small_runs_loc=r"Q:\Queueing_sys\Small_runs"
 simworking_loc=r"Q:\Queueing_sys\working_loc"
 admin_loc=r"Q:\Queueing_sys\admin"
@@ -36,14 +39,17 @@ priority_loc=r"Q:\Queueing_sys\Priority"
 normal_loc=r"Q:\Queueing_sys\Normal"
 long_loc=r"Q:\Queueing_sys\Long"
 completed_loc=r"Q:\Queueing_sys\Completed"
-single_cl_mode_loc=r"Q:\Queueing_sys\admin\onlyone.txt"
-hostlist_32=r'-par-dist "IN-PU-1040*8, IN-PU-1001*8,IN-PU-1038*8 IN-PU-1002*8"'
+MP_completed_loc=r"Q:\Queueing_sys\MNP_Completed"
+hostlist_32=r'-par-dist "SLAVE1*8, SLAVE2*8, SLAVE3*8, MASTER*8"'
 ansys_inst_loc=r'C:\Program Files\ANSYS Inc\v145\CFX\bin\cfx5solve'
 
-def move_command(file_loc1, file_loc2,current_location):
-    os.chdir(current_location)    
+def move_command(file_loc1, file_loc2):
     print "Moving File %s to %s .. .." %(file_loc1, file_loc2)
     os.system ("move %s %s" %(file_loc1,file_loc2))
+	
+def move_folder(loc1,loc2):
+	print "Moving Folder %s to %s....."%(loc1,loc2)
+	sh.move(loc1, loc2)
 
 #Creating a log/status file to show which run is currently going on.
 def R_logger(variable):
@@ -54,7 +60,6 @@ def R_logger(variable):
     run_writer.write("<b> Ongoing Run Start:</b><br/>%s<br/><br/>%s"%(dt_log,run_name[1]))
     run_writer.close()
     os.system
-    
 #Creating a log/status file to show which runs are pending.
 def Q_logger(variable1,variable2):
     logf_loc=os.path.join(logger_loc,'Queue_%s.html'%variable2)
@@ -72,12 +77,12 @@ def Q_logger(variable1,variable2):
 
 
 #Write individual BAT scripts for each def file and run it
-def run_exec(variable):
+def run_exec(variable,v2,v3):
     fbw=r'"%s"'%(variable)
     dt_log="%s_%s" %(dt.date.today(),t.strftime("%HHr-%Mmin-%Ssec"))
-    temp2=os.path.join(simworking_loc,'job_%s_CL2.BAT'%dt_log)
+    temp2=os.path.join(v2,'job_%s_CL1.BAT'%dt_log)
     f=open(temp2,'w')
-    f.write('rem Start of BAT File\n@Echo off\nSet ANSWAIT=1\ncd "%s"\n"%s" -def %s %s'%(simworking_loc,ansys_inst_loc,fbw,hostlist_32))
+    f.write('rem Start of BAT File\n@Echo off\nSet ANSWAIT=1\ncd "%s"\n"%s" -%s %s %s'%(v2,ansys_inst_loc,v3,fbw,hostlist_32))
     f.close()
     sp.call(r'"%s"'%temp2)
     t.sleep(1)
@@ -89,7 +94,7 @@ def list_creator_normal(variable1, variable2):
     logger_list_loc=os.path.join(logger_loc,list_name)
     variableA1=os.path.join(variable1,'list_creator_CL1.BAT')
     BC=open('%s'%variableA1,'w')
-    BC.write('@Echo off\ncd /d " "%s"\ndir *.def /OD /b >"%s"'%(variable1,admin_list_loc))
+    BC.write('@Echo off\ncd /d " "%s"\ndir *.def *.mdef /OD /b /s >"%s"'%(variable1,admin_list_loc))
     BC.close()
     t.sleep(3)
     os.popen('"%s"'%variableA1)
@@ -101,18 +106,21 @@ def list_creator_normal(variable1, variable2):
     if fnames1==[]:
         print "Normal List is empty. Taking a Long status run"        
         list_creator_long(long_loc,'Long')
-    else :
-        main_prog(fnames1[0],normal_loc)
+    fchk=re.search(r'Q:\\Queueing_sys\\Normal\\MNP_Input', fnames1[0], flags=re.IGNORECASE)
+    if fchk:
+        MP_prog(fnames1[0])
+    else:
+        main_prog(fnames1[0])
 
 #Creating BAT script for extracting the list of def files in the priority folder - sorted Date wise. If list is empty - move to Normal Folder
 def list_creator_priority(variable1, variable2):
     print"variable 1 is %s"%variable1
     list_name='List_%s.txt'%variable2
     list_loc=os.path.join(admin_loc,list_name)
-    variableA1=os.path.join(variable1,'list_creator_CL2.BAT')
+    variableA1=os.path.join(variable1,'list_creator_CL1.BAT')
     print variableA1
     BC=open('%s'%variableA1,'w')
-    BC.write('@Echo off\ncd /d " "%s"\ndir *.def /OD /b /s >"%s"'%(variable1,list_loc))
+    BC.write('@Echo off\ncd /d " "%s"\ndir *.def *.mdef /OD /b /s >"%s"'%(variable1,list_loc))
     BC.close()
     t.sleep(2)
     os.popen('"%s"'%variableA1)
@@ -126,17 +134,17 @@ def list_creator_priority(variable1, variable2):
         print "priority List is empty"
         list_creator_normal(normal_loc,'Normal')
     else :
-        main_prog(fnames1[0],priority_loc)
+        main_prog(fnames1[0])
 
 #List Creator for small runs. Checks file Size before proceeding..        
 def list_creator_smallruns(variable1, variable2):
     print"variable 1 is %s"%variable1
     list_name='List_%s.txt'%variable2
     list_loc=os.path.join(admin_loc,list_name)
-    variableA1=os.path.join(variable1,'list_creator_CL2.BAT')
+    variableA1=os.path.join(variable1,'list_creator_CL1.BAT')
     print variableA1
     BC=open('%s'%variableA1,'w')
-    BC.write('@Echo off\ncd /d " "%s"\ndir *.def /OD /b /s >"%s"'%(variable1,list_loc))
+    BC.write('@Echo off\ncd /d " "%s"\ndir *.def *.mdef /OD /b /s >"%s"'%(variable1,list_loc))
     BC.close()
     t.sleep(2)
     os.popen('"%s"'%variableA1)
@@ -152,59 +160,26 @@ def list_creator_smallruns(variable1, variable2):
     else :
         if (os.stat("%s"%fnames1[0]).st_size<180000000):
             print "Small run file size is okay. Processing..."
-            main_prog(fnames1[0], small_runs_loc)            
+            main_prog(fnames1[0])            
         else :
             print "Small Run File Size is Excess. Moving out.."            
-            move_command(fnames1[0], excess_size_loc, small_runs_loc)
+            move_command(fnames1[0], excess_size_loc)
             print "Exiting.."
             t.sleep(1)
-            print "Deleting Scan flag"        
-            flag_del('CL2_scan.txt')
             exit()
-  from random import choice, randint
-    
-## Beat poetry
-
-adjectives = "radiant bright ecstatic young lithe driving running splitting".split()
-verbs = "wriggles grows stares saunters drives smiles raps".split()
-nouns = "pen inkwell willow sky station train pane window sunlight dust".split()
-
-def beat_poem():
-    print "\n" + "="*16+" a poem " + "="*16
-    for x in range(randint(5,11)):
-        words1 = " ".join([choice(adjectives) + ", ", choice(adjectives)])
-        words2 = " ".join([choice(nouns), choice(verbs)])
-        words3 = " ".join([choice(nouns), choice(nouns), choice(adjectives), choice(nouns)])
-        
-        for i in range(randint(2,5)):
-            words = choice([words1, words2, words3])
-            line = " "*randint(0, 40 - len(words)) + words
-            print line
-        
-        if x % 3 ==0:
-            print
-            print " "*5 + choice(nouns)
-            print " "*5 + choice(nouns)
-            print " "*5 + "the " + choice(nouns) +"!"
-            print
-        
-        if x % 7 ==0:
-            words4 = " ".join(["the " + choice(adjectives), choice(nouns), choice(verbs)])
-            print
-            print " ".join(words4)
-            print
-beat_poem()
+            
+            
 #List Creator for Long Status Runs. Runs only if Small runs, priority and normal lists are empty.
 def list_creator_long(variable1, variable2):
     print"variable 1 is %s"%variable1
     list_name='List_%s.txt'%variable2
     list_loc=os.path.join(admin_loc,list_name)
-    variableA1=os.path.join(variable1,'list_creator_CL2.BAT')
+    variableA1=os.path.join(variable1,'list_creator_CL1.BAT')
     print variableA1
     BC=open('%s'%variableA1,'w')
-    BC.write('@Echo off\ncd /d " "%s"\ndir *.def /OD /b /s >"%s"'%(variable1,list_loc))
+    BC.write('@Echo off\ncd /d " "%s"\ndir *.def *.mdef /OD /b /s >"%s"'%(variable1,list_loc))
     BC.close()
-    t.sleep(2)
+    t.sleep(1)
     os.popen('"%s"'%variableA1)
     t.sleep(2)
     LC=open(list_loc,'r')
@@ -214,13 +189,12 @@ def list_creator_long(variable1, variable2):
     print "Long status list is %s"%fnames1
     if fnames1==[]:
         print "Long List is empty"
-        print "Deleting Scan flag"        
-        flag_del('CL2_scan.txt')
+        flag_del('CL1_scan.txt',admin_loc)
         exit()
     else :
-        main_prog(fnames1[0],long_loc)
+        main_prog(fnames1[0])
 
-          
+            
 #Reading the list generated and passing the first def file to the main program.
 def list_reader(variable1):
     f = open("%s"%(fdiff_loc),'r')
@@ -230,7 +204,7 @@ def list_reader(variable1):
 
 #Function for deleting the older BAT files used to execute the simulation. Prevents Clutter.
 def BAT_killer(variable):
-    for variable in glob.glob('%s/*_CL2.BAT'%variable):
+    for variable in glob.glob('%s/*_CL1.BAT'%variable):
         if os.path.isfile(variable):
             os.remove(variable)
             
@@ -238,50 +212,80 @@ def BAT_killer(variable):
 def Scan_race(variable,var2):
     for variable in glob.glob('%s/%s_scan.txt'%(variable,var2)):
         if os.path.isfile(variable):
-            print "Race exists. Pausing for 25 secs"
-            t.sleep(22)
-            #exit()
+            print "Race exists. Exiting Program"
+            flag_del('CL1_scan.txt',admin_loc)           
+            exit()
 
 #Creating and deleting Flag for showing the simulation is running.
-def flag_create(variable):
+def flag_create(variable,v2):
     print "Creating the Flag for simulation run"
-    stat_loc=os.path.join(admin_loc,variable)
+    stat_loc=os.path.join(v2,variable)
     f=open(stat_loc,'w')
     f.write("Running")
     f.close()
 
-def flag_del(variable):
+def flag_del(variable,v2):
     print "Deleting the Flag for simulation run"
-    stat_loc=os.path.join(admin_loc,variable)
+    stat_loc=os.path.join(v2,variable)
     os.remove(stat_loc)
 
+def MP_prog(variable):
+    print "Entering MP_Prog"    
+    BAT_killer(MP_working_loc)
+    loc1='"%s"'%variable
+    loc2='"%s"'%MP_working_loc
+    temp1=os.path.split(variable)
+    runner_name=os.path.join(MP_working_loc,temp1[1])
+    move_command(loc1,loc2)
+    print "\n\nFinished Moving files"
+    flag_del('CL1_scan.txt',admin_loc)
+    flag_create('status1_MP.txt',admin_loc)
+    R_logger(variable)
+    run_exec(runner_name,MP_working_loc)
+    t.sleep(1)
+    flag_del('status1_MP.txt',admin_loc)
+    BAT_killer(MP_working_loc)
+    temp2=os.path.splitext(temp1[1])[0]
+    success_scan(MP_working_loc,temp2,'res',MP_completed_loc)
+    if success_scan.scanperm=='yes':
+        success_scan(MP_working_loc,temp2,'out',MP_completed_loc)
+        success_scan(MP_working_loc,temp2,'def',MP_completed_loc)
+    else: print "Since Res file unavailable. Leaving def and out file in working_loc..."
+    
+
 #Main Program - gets variable (Def file name) from Priority first and if empty, from Normal
-def main_prog(variable,v2):
+def main_prog(variable):        
     BAT_killer(simworking_loc)
     loc1='"%s"'%variable
     loc2='"%s"'%simworking_loc
     temp1=os.path.split(variable)
+    fextension=os.path.splitext(temp1[1])[1][1:]
     runner_name=os.path.join(simworking_loc,temp1[1])
-    move_command(loc1,loc2,v2)
-    print "\n\nFinished Moving files"
-    flag_del('CL2_scan.txt')
-    flag_create('status2.txt')
+    move_command(loc1,loc2)
+    print "\n\nFinished Moving file"
+    if fextension=='mdef':
+        foldpath=variable.split(".")[0]
+        move_folder(foldpath,simworking_loc)
+        print "\n\n Finished moving Folder for mdef.."
+    flag_del('CL1_scan.txt',admin_loc)
+    flag_create('status1.txt',admin_loc)
     R_logger(variable)
-    run_exec(runner_name)
+    run_exec(runner_name,simworking_loc,fextension)
     t.sleep(1)
-    flag_del('status2.txt')
+    flag_del('status1.txt',admin_loc)
     BAT_killer(simworking_loc)
     temp2=os.path.splitext(temp1[1])[0]
-    success_scan(simworking_loc,temp2,'res')
+    success_scan(simworking_loc,temp2,'res',completed_loc)
     if success_scan.scanperm=='yes':
-        success_scan(simworking_loc,temp2,'out')
-        success_scan(simworking_loc,temp2,'def')
+        success_scan(simworking_loc,temp2,'out',completed_loc)
+        success_scan(simworking_loc,temp2,'def',completed_loc)
+        success_scan(simworking_loc,temp2,'mdef',completed_loc)
     else: print "Since Res file unavailable. Leaving def and out file in working_loc..."
-
+	
 #Scans for res file matching the def file name and then an out file.
 #If available, the res,def and out files are transfered to the completed folder.  
 #If res file unavailable, def and out files remain in working_loc
-def success_scan(variable1, variable2, variable3):
+def success_scan(variable1, variable2, variable3, v4):
     p2=glob.glob('%s/%s*.%s' %(variable1,variable2, variable3))
     #print p2
     if p2==[]:
@@ -291,7 +295,7 @@ def success_scan(variable1, variable2, variable3):
     else:
         success_scan.scanperm='yes'        
         print "the matching %s file is %s" %(variable3,p2[0])
-        move_command(p2[0],completed_loc,variable1)
+        move_command(p2[0],v4)
         return success_scan.scanperm
 
 #Program first checks if the Pause flag is up. If yes - program exits. 
@@ -299,24 +303,23 @@ def success_scan(variable1, variable2, variable3):
 fexist1=os.path.isfile('%s'%pause_set_loc)
 print "Pause Status Set ?%s" %fexist1
 if (fexist1==True):
-    print "Pause for CL 2 Request available. Exiting program"
+    print "Pause CL1 Request available. Exiting program"
     exit()
 else:
-    print "Pause for CL2 setting is Off. Checking Run Status.."
+    print "Pause CL1 setting is Off. Checking Run Status.."
     
 fexist2=os.path.isfile('%s'%status_loc)
-print "Status 2 File Exists? %s" %fexist2
-#print "Checking for Single Cluster Mode and Cluster 1 status"
-fexist3=os.path.isfile('%s'%single_cl_mode_loc)
-fexist4=os.path.isfile('%s'%status_other_loc)
+fexist3=os.path.isfile('%s'%single_cluster_switch)
+print "Status File Exists? %s" %fexist2
 if (fexist2==True):
-    print "Exiting program as status is active."
+    print "Exiting program"
     exit()
 else:
-    print "Good to go. Creating Current Scan Flag.."
-    t.sleep(5)
-    flag_create('CL2_scan.txt')
+    print "Single CLuster mode is :%s" %fexist3
+    print "Creating Current Scan Flag.."
+    flag_create('CL1_scan.txt',admin_loc)
     print "Checking for Race Condition..."
-    Scan_race(admin_loc,'CL1')
+    Scan_race(admin_loc,'CL2')
     print "Run can start. Going to the Small Runs Priority folder..."
     list_creator_smallruns(small_runs_loc,'Small Run Priority')
+    
